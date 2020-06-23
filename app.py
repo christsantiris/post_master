878b534d631ae12727774b78a0a8a0579921bc26
Chris Tsantiris <chris.tsantiris@gmail.com>
2020-06-23 15:49:28 -0400

    add route for posts

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/posts')
+def posts():
+    return render_template('posts.html')
+
 if __name__ == '__main__':
     app.run(debug=True)

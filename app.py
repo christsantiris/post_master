1a6ef5b57ffaf738476f1699f497cca93ad3cf80
Chris Tsantiris <chris.tsantiris@gmail.com>
2020-06-23 15:14:04 -0400

    only allow get against home route

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/home')
-def hello():
-    return "Hello World"
+@app.route('/home/<string:name>', methods=['GET'])
+def hello(name):
+    return 'Hello, ' + name
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)

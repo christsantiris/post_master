c74d1a99e53c68cddc2a79c7add390b422fff373
Chris Tsantiris <chris.tsantiris@gmail.com>
2020-06-24 10:50:13 -0400

    display author if author

--- app.py
+++ app.py
@@ -2,13 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+all_posts = [
+  {
+    'title': 'Post 1',
+    'content': 'This is post 1',
+    'author': 'Chris'
+  },
+    {
+    'title': 'Post 2',
+    'content': 'This is post 2'
+  },
+    {
+    'title': 'Post 3',
+    'content': 'This is post 3'
+  }
+]
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/posts')
 def posts():
-    return render_template('posts.html')
+    return render_template('posts.html', posts=all_posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
